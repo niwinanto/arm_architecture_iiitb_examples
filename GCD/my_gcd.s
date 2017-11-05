e4a4e87165cc937a02e9aa79b7afc1404425246a
niwinanto <niwinantop@gmail.com>
2017-11-05 23:06:22 +0530

    Create my_gcd.s

---
+++ GCD/my_gcd.s
@@ -0,0 +1,18 @@
+	AREA myCode, CODE
+		ENTRY
+		EXPORT __main
+			
+__main
+		MOV r1, #45
+		MOV r2, #15
+		B gcd		
+loop	CMP r1, r2		;compare r1 and r2 
+		ITE GT 			
+		SUBGT r1, r1, r2	;if true 
+		SUBLE r2, r2, r1	;else	
+gcd		CMP r1, r2			;compare r1 and r1 
+		BNE loop			;jump to loop on not equal
+stop	B stop
+		END
+		
+		
