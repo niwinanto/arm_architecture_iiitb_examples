4835e8aab941d1a484ba0497231f5d6f4e755fe5
niwinanto <niwinantop@gmail.com>
2017-12-06 10:10:59 +0530

    Update output.s

--- ARM_compiler/output.s
+++ ARM_compiler/output.s
@@ -1,1 +1,20 @@
+;****************CODE STARTS*****************
+AREA myCode, CODE
+	ENTRY
+	EXPORT __main
 
+
+__main FUNCTION
+
+	MOV R0, #1
+	PUSH {R0}
+	MOV R0, #3
+	PUSH {R0}
+	POP {R0, R1}
+	ADD R0, R0, R1
+	PUSH {R0}
+
+stop 	B stop
+	ENDFUNC
+	END
+;****************CODE ENDS********************
